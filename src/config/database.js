64136fbc8a6eb39b821f69bc16adb4e7ba6090d5
antoniojunioalves <antoniojunioalves@gmail.com>
2019-07-12 17:31:16 -0300

    Correção para conectar no mongoDB online.

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise // Feito somente para tirar a advertência 
-module.exports = mongoose.connect('mongodb://localhost/todo')
-
+let stringConnectionDB = `mongodb+srv://${process.env.USER_MONGO}:${process.env.PASSWORD_MONGO}@cluster0-afjvm.mongodb.net/test?retryWrites=true&w=majority`
+//module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose
+    .connect(stringConnectionDB, { useNewUrlParser: true })
+    .then(() => {
+        console.log("[DATABASE] - Mongo is connected");
+    })
+    .catch(err => {
+        console.log("[DATABASE] - Error on Mongo connection");
+    });
